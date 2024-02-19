8876b6dc43b19d52742b11bdfa07b537e0089bc4
Katia Muñoz <108428433+Negativekatiax@users.noreply.github.com>
2024-02-18 21:23:49 -0600

    Intestigacion y codigo de cada metodo

---
+++ Seleccion.java
@@ -0,0 +1,37 @@
+public class Seleccion {
+    
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Arreglo original:");
+        printArray(arr);
+        selectionSort(arr);
+        System.out.println("Arreglo ordenado:");
+        printArray(arr);
+    }
+}
+
